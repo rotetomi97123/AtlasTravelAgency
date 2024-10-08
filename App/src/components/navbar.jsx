8232896b74038ae3f89b5f736d72b4f8d63d683a
toth <tot.tamas04@gmail.com>
2024-10-08 18:21:41 +0200

    nav fixing

--- App/src/components/navbar.jsx
+++ App/src/components/navbar.jsx
@@ -10,7 +10,9 @@ const navbar = () => {
         <img src={logo} alt="logo" className="logo" />
         <ul className="nav-menu">
           <li className="nav-item nav_dashed">
-            Putovanja <span>za sve generacije</span>
+            <div className="nav_hover">
+              Putovanja <span>za sve generacije</span>
+            </div>
             <ul className="dropdown">
               {Object.keys(data.putovanje_za_sve).map((categoryKey) => (
                 <div key={categoryKey} className="category">
@@ -28,7 +30,9 @@ const navbar = () => {
             </ul>
           </li>
           <li className="nav-item nav_dashed">
-            Putovanja <span>za mlade</span>{" "}
+            <div className="nav_hover">
+              Putovanja <span>za mlade</span>{" "}
+            </div>
             <ul className="dropdown">
               {Object.keys(data.putovanje_za_mlade).map((categoryKey) => (
                 <div key={categoryKey} className="category">
@@ -46,8 +50,8 @@ const navbar = () => {
             </ul>
           </li>
           <li className="nav-item">
-            Leto 2025{" "}
-            <ul className="dropdown">
+            <div className="nav_hover">Leto 2025</div>
+            <ul className="dropdown dropdown_leto">
               {Object.keys(data.leto_2025).map((categoryKey) => (
                 <div key={categoryKey} className="category">
                   <h2>{categoryKey.replaceAll("_", " ")}</h2>
@@ -64,10 +68,19 @@ const navbar = () => {
             </ul>
           </li>
           <li className="nav-item">
-            Hoteli{" "}
+            <div className="nav_hover">Hoteli</div>
             <ul className="dropdown dropdown_smaller">
               {Object.keys(data.hoteli).map((categoryKey) => (
-                <div key={categoryKey} className="category">
+                <div
+                  key={categoryKey}
+                  className={`category ${
+                    categoryKey === "hoteli1"
+                      ? "hotel1-class"
+                      : categoryKey === "hoteli2"
+                      ? "hotel2-class"
+                      : ""
+                  }`}
+                >
                   <ul className="category_ul">
                     {data.hoteli[categoryKey].map((trip, index) => (
                       <li key={index} className="trip">
@@ -80,11 +93,26 @@ const navbar = () => {
             </ul>
           </li>
           <li className="nav-item grey_bg">
-            Poslovnice <span>Kontakti</span>
-            <ul className="dropdown">
-              <li className="dropdown-item">Service 1</li>
-              <li className="dropdown-item">Service 2</li>
-              <li className="dropdown-item">Service 3</li>
+            <div className="nav_hover">
+              Poslovnice <span>Kontakti</span>
+            </div>
+            <ul className="dropdown dropdown_contact">
+              <div>
+                <h2>Radno vreme:</h2>
+                <h5>Pon-Pet 10-19h, Sub 10-16h</h5>
+                <p>
+                  <strong>011 7450 444 /</strong> centrala <br></br> Dostupni
+                  smo i na<br></br>
+                  <span className="viber">Viber</span> i
+                  <span className="whatsApp"> WhatsApp</span>
+                </p>
+                <strong>Balkanska 27, Beograd</strong>
+              </div>
+              <div className="contact_links">
+                <a href="/">O nama</a>
+                <a href="/">Kontakt</a>
+                <a href="/">Provera duga</a>
+              </div>
             </ul>
           </li>
         </ul>
