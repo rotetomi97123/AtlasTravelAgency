b9201957f7c2f50f91d395221de1d827c0308163
toth <tot.tamas04@gmail.com>
2024-10-10 22:34:05 +0200

    nav fixes,hero section

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Hero from "./components/hero";
 import "./css/style.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
--- App/src/components/navbar.jsx
+++ App/src/components/navbar.jsx
@@ -3,9 +3,10 @@ import "../css/style.css";
 import logo from "../assets/logo.svg";
 import data from "../data.json";
 import hamburger from "../assets/hamburger.svg";
-import close from "../assets/close.svg";
+import close from "../assets/close3.svg";
 import arrow from "../assets/arrow.svg";
 import arrow_down from "../assets/arrow_down.svg";
+import arrow_right from "../assets/arrow_right.svg";
 
 const navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -135,75 +136,145 @@ const navbar = () => {
         </div>
         {isMobile && (
           <div className="mobile_nav_dropdown">
-            <div
-              className="mobile_nav_dropdown_wrapper"
-              onClick={() =>
-                setToggleDropdown((prev) => (prev === "first" ? "" : "first"))
-              }
-            >
-              <h3>Putovanja za sve generacije</h3>
-              {toggleDropdown === "first" ? (
-                <img src={arrow} alt="arrow" />
-              ) : (
-                <img src={arrow_down} alt="arrow" />
+            <div className="mobile_nav_dropdown_padding">
+              <div
+                className="mobile_nav_dropdown_wrapper"
+                onClick={() =>
+                  setToggleDropdown((prev) => (prev === "first" ? "" : "first"))
+                }
+              >
+                <h3>Putovanja za sve generacije</h3>
+                {toggleDropdown === "first" ? (
+                  <img src={arrow} alt="arrow" />
+                ) : (
+                  <img src={arrow_down} alt="arrow" />
+                )}
+              </div>
+              {toggleDropdown === "first" && (
+                <ul className="mobile_nav_dropdown_content">
+                  {Object.keys(data.putovanje_za_sve).map((categoryKey) => (
+                    <div
+                      key={categoryKey}
+                      className="mobile_nav_dropdown_category"
+                    >
+                      <h2>{categoryKey.replaceAll("_", " ")}</h2>{" "}
+                      <ul className="">
+                        {data.putovanje_za_sve[categoryKey].map(
+                          (trip, index) => (
+                            <li key={index}>
+                              <h5>{trip.title}</h5>
+                              <p>od {trip.startingPrice}€</p>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </div>
+                  ))}
+                </ul>
+              )}
+              <div
+                className="mobile_nav_dropdown_wrapper"
+                onClick={() =>
+                  setToggleDropdown((prev) =>
+                    prev === "second" ? "" : "second"
+                  )
+                }
+              >
+                <h3>Putovanja za mlade</h3>
+                {toggleDropdown === "second" ? (
+                  <img src={arrow} alt="arrow" />
+                ) : (
+                  <img src={arrow_down} alt="arrow" />
+                )}{" "}
+              </div>
+              {toggleDropdown === "second" && (
+                <ul className="mobile_nav_dropdown_content">
+                  {Object.keys(data.putovanje_za_mlade).map((categoryKey) => (
+                    <div
+                      key={categoryKey}
+                      className="mobile_nav_dropdown_category"
+                    >
+                      <h2>{categoryKey.replaceAll("_", " ")}</h2>{" "}
+                      <ul className="">
+                        {data.putovanje_za_mlade[categoryKey].map(
+                          (trip, index) => (
+                            <li key={index}>
+                              <h5>{trip.title}</h5>
+                              <p>od {trip.startingPrice}€</p>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </div>
+                  ))}
+                </ul>
+              )}
+              <div
+                className="mobile_nav_dropdown_wrapper"
+                onClick={() =>
+                  setToggleDropdown((prev) => (prev === "third" ? "" : "third"))
+                }
+              >
+                <h3>Leto 2025</h3>
+                {toggleDropdown === "third" ? (
+                  <img src={arrow} alt="arrow" />
+                ) : (
+                  <img src={arrow_down} alt="arrow" />
+                )}{" "}
+              </div>
+              {toggleDropdown === "third" && (
+                <ul className="mobile_nav_dropdown_content">
+                  {Object.keys(data.leto_2025).map((categoryKey) => (
+                    <div
+                      key={categoryKey}
+                      className="mobile_nav_dropdown_category"
+                    >
+                      <h2>{categoryKey.replaceAll("_", " ")}</h2>{" "}
+                      <ul className="">
+                        {data.leto_2025[categoryKey].map((trip, index) => (
+                          <li key={index}>
+                            <h5>{trip.title}</h5>
+                            <p>od {trip.startingPrice}€</p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </ul>
               )}
             </div>
-            {toggleDropdown === "first" && (
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Nostrum esse eius voluptatem, rem libero deserunt ducimus
-                  rerum omnis aperiam provident illo assumenda tempora quibusdam
-                  ipsa hic necessitatibus, error inventore. Laudantium?
-                </p>
+            <div className="mobile_nav_dropdown_contact">
+              <div className="mobile_nav_dropdown_contact_heading">
+                Poslovnice Kontakti
               </div>
-            )}
-            <div
-              className="mobile_nav_dropdown_wrapper"
-              onClick={() =>
-                setToggleDropdown((prev) => (prev === "second" ? "" : "second"))
-              }
-            >
-              <h3>Putovanja za mlade</h3>
-              {toggleDropdown === "second" ? (
-                <img src={arrow} alt="arrow" />
-              ) : (
-                <img src={arrow_down} alt="arrow" />
-              )}{" "}
+              <ul className="mobile_nav_dropdown_contact_content">
+                <div>
+                  <h2>Radno vreme:</h2>
+                  <h5>Pon-Pet 10-19h, Sub 10-16h</h5>
+                  <p>
+                    <strong>011 7450 444 /</strong> centrala <br></br> Dostupni
+                    smo i na<br></br>
+                    <span className="viber">Viber</span> i
+                    <span className="whatsApp"> WhatsApp</span>
+                  </p>
+                  <strong>Balkanska 27, Beograd</strong>
+                </div>
+                <div className="contact_links">
+                  <div className="contact_links_wrapper">
+                    <a href="/">O nama</a>
+                    <img src={arrow_right} alt="arrow" />
+                  </div>
+                  <div className="contact_links_wrapper">
+                    <a href="/">Kontakt</a>
+                    <img src={arrow_right} alt="arrow" />
+                  </div>
+                  <div className="contact_links_wrapper">
+                    <a href="/">Provera duga</a>
+                    <img src={arrow_right} alt="arrow" />
+                  </div>
+                </div>
+              </ul>
             </div>
-            {toggleDropdown === "second" && (
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Nostrum esse eius voluptatem, rem libero deserunt ducimus
-                  rerum omnis aperiam provident illo assumenda tempora quibusdam
-                  ipsa hic necessitatibus, error inventore. Laudantium?
-                </p>
-              </div>
-            )}
-            <div
-              className="mobile_nav_dropdown_wrapper"
-              onClick={() =>
-                setToggleDropdown((prev) => (prev === "third" ? "" : "third"))
-              }
-            >
-              <h3>Leto 2025</h3>
-              {toggleDropdown === "third" ? (
-                <img src={arrow} alt="arrow" />
-              ) : (
-                <img src={arrow_down} alt="arrow" />
-              )}{" "}
-            </div>
-            {toggleDropdown === "third" && (
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Nostrum esse eius voluptatem, rem libero deserunt ducimus
-                  rerum omnis aperiam provident illo assumenda tempora quibusdam
-                  ipsa hic necessitatibus, error inventore. Laudantium?
-                </p>
-              </div>
-            )}
           </div>
         )}
       </nav>
